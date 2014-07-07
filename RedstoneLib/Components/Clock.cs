c44c8e858085bbbbfdef42871192686bced852ce
DvdKhl <dvdkhl@gmail.com>
2014-07-07 23:25:30 +0200

    Added new RSComponent: Clock

---
+++ RedstoneLib/Components/Clock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedstoneLib.Components {
+	public class Clock : RSComponent {
+		public RSConnection Output { get; private set; }
+
+		private bool enabled;
+		public bool Enabled { get { return enabled; } set { enabled = value; EnabledChangedHandler(); } }
+
+		public int LowPowerLevel { get; set; }
+		public int HighPowerLevel { get; set; }
+		public int HighWidth { get; set; }
+		public int LowWidth { get; set; }
+
+		public Clock(RSEngine engine) : base(engine) {
+			Output = CreateOutput("Out");
+			ScheduleAction(EnabledChangedHandler, CurrentTick + 1);
+
+			LowPowerLevel = 0;
+			HighPowerLevel = RSEngine.MaxPowerLevel;
+		}
+
+		private bool isLow = false;
+		private void EnabledChangedHandler() {
+			if(!enabled) return;
+
+			ScheduleStimulus(Output, isLow ? LowPowerLevel : HighPowerLevel);
+			ScheduleAction(EnabledChangedHandler, CurrentTick + (isLow ? LowWidth : HighWidth));
+			isLow = !isLow;
+		}
+
+	}
+}
