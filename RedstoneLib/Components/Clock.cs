e7d01f19775b5960a955c5719008bdbb48407838
DvdKhl <dvdkhl@gmail.com>
2014-07-08 02:01:56 +0200

    Fixed Clock component

--- RedstoneLib/Components/Clock.cs
+++ RedstoneLib/Components/Clock.cs
@@ -9,27 +9,41 @@ namespace RedstoneLib.Components {
 		public RSConnection Output { get; private set; }
 
 		private bool enabled;
-		public bool Enabled { get { return enabled; } set { enabled = value; EnabledChangedHandler(); } }
+		public bool Enabled { get { return enabled; } set { enabled = value; ScheduleUpdate(); } }
 
 		public int LowPowerLevel { get; set; }
 		public int HighPowerLevel { get; set; }
 		public int HighWidth { get; set; }
 		public int LowWidth { get; set; }
 
-		public Clock(RSEngine engine) : base(engine) {
+		public Clock(RSEngine engine)
+			: base(engine) {
 			Output = CreateOutput("Out");
-			ScheduleAction(EnabledChangedHandler, CurrentTick + 1);
 
 			LowPowerLevel = 0;
 			HighPowerLevel = RSEngine.MaxPowerLevel;
+
+			ScheduleUpdate();
 		}
 
 		private bool isLow = false;
-		private void EnabledChangedHandler() {
-			if(!enabled) return;
+		private bool isActionScheduled = false;
+
+		private void ScheduleUpdate() {
+			if(!enabled || isActionScheduled) return;
+			ScheduleAction(UpdateState, CurrentTick + 1);
+			isActionScheduled = true;
+		}
+
+		private void UpdateState() {
+			if(!enabled) {
+				isActionScheduled = false;
+				return;
+			}
 
 			ScheduleStimulus(Output, isLow ? LowPowerLevel : HighPowerLevel);
-			ScheduleAction(EnabledChangedHandler, CurrentTick + (isLow ? LowWidth : HighWidth));
+
+			ScheduleAction(UpdateState, CurrentTick + (isLow ? LowWidth : HighWidth));
 			isLow = !isLow;
 		}
 
