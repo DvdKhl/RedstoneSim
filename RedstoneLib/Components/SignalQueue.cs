91c4f5f7fe2d622ea1d2e7b1ca2a7b534998a1fb
DvdKhl <dvdkhl@gmail.com>
2014-07-09 10:37:54 +0200

    Added SignalQueue -Useraddable powerlevel sequences (append) -At each tick outputs the next powerlevel in the sequence

---
+++ RedstoneLib/Components/SignalQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedstoneLib.Components {
+	public class SignalQueue : RSComponent {
+
+		public RSConnection Output { get; private set; }
+
+		private Queue<int> memory = new Queue<int>();
+
+		public SignalQueue(RSEngine engine)
+			: base(engine) {
+			Output = CreateOutput("Out");
+
+		}
+
+		public void AddSequence(int[] seq) {
+			if(seq.Length == 0) return;
+
+			if(memory.Count == 0) ScheduleAction(UpdateState, CurrentTick + 1);
+			foreach(var item in seq) memory.Enqueue(item);
+		}
+
+		private long lastUpdateTick;
+		private void UpdateState() {
+			if(lastUpdateTick == CurrentTick) return;
+			lastUpdateTick = CurrentTick;
+
+			ScheduleStimulus(Output, memory.Dequeue());
+			if(memory.Count != 0) ScheduleAction(UpdateState, CurrentTick + 1);
+		}
+
+	}
+}
